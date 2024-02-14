cc549836b3cec668f82405c9a06abe41b44cb797
PhasitWo <vrzoearth@gmail.com>
2024-02-15 01:42:13 +0700

    client css

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 function Home({ setPage }) {
-    function joinRoom() {
+    function joinRoom(e) {
+        e.preventDefault()
         const loadModal = document.getElementById("load-modal");
         loadModal.showModal();
         setTimeout(() => {
@@ -10,31 +11,36 @@ function Home({ setPage }) {
         }, 2000);
     }
 
+    function closeJoinModal() {
+        document.getElementById("join-modal").close();
+    }
+
     return (
         <>
-            <h1>This is Home Page</h1>
-            <button onClick={() => document.getElementById("wait-modal").showModal()}>Create Room</button>
-            <br />
-            <button onClick={() => document.getElementById("join-modal").showModal()}>Join Room</button>
-            <dialog className="join-modal" id="join-modal">
-                This is Join Dialog!
-                <form method="dialog">
-                    <label htmlFor="code">Code </label>
-                    <input id="code"></input>
-                    <br />
-                    <button type="button" onClick={joinRoom}>Join</button>
-                    <button>Close</button>
-                </form>
-            </dialog>
-            <dialog className="wait-modal" id="wait-modal">
-                Waiting for other player!
-                <form method="dialog">
-                    <button>Quit</button>
-                </form>
-            </dialog>
-            <dialog className="load-modal" id="load-modal">
-                LOADING!
-            </dialog>
+            <div className="home-container">
+                <h2>Rock Paper Scissors</h2>
+                <button onClick={() => document.getElementById("wait-modal").showModal()}>Create Room</button>
+                <br />
+                <button onClick={() => document.getElementById("join-modal").showModal()}>Join Room</button>
+                <dialog className="join-modal" id="join-modal">
+                    <p>This is Join Dialog!</p>
+                    <form onSubmit={joinRoom} method="dialog">
+                        <label htmlFor="code">Code </label>
+                        <input id="code"></input>
+                        <button type="submit">Join</button>
+                        <button type="button" onClick={closeJoinModal}>Close</button>
+                    </form>
+                </dialog>
+                <dialog className="wait-modal" id="wait-modal">
+                    Waiting for other player!
+                    <form method="dialog">
+                        <button>Quit</button>
+                    </form>
+                </dialog>
+                <dialog className="load-modal" id="load-modal">
+                    LOADING...
+                </dialog>
+            </div>
         </>
     );
 }
---
+++ client/src/client.js
@@ -0,0 +1,6 @@
+// var socket = io("http://localhost:3000");
+// var currentRoomCode = null;
+
+// socket.io.on("error", (error) => {
+//     popError("Cannot Connect to Server")
+// });
